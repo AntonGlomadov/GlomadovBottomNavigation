e952decf93171782e7fb7614f6cb30aff215bd0d
Anton <AGlomadov@stud.kantiana.ru>
2021-09-29 20:17:32 +0200

    Navigation

---
+++ app/src/main/java/com/example/glomadovbottomnavigation/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.glomadovbottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.glomadovbottomnavigation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setupBottomNavigationBar()
+        setContentView(binding.root)
+    }
+    private fun setupBottomNavigationBar() {
+        binding.botNav.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_events -> {openFragment(EventsFragment())
+                    true}
+                R.id.navigation_holidays -> {openFragment(HolidaysFragment())
+                    true}
+                R.id.navigation_profile -> {openFragment(HomeFragment())
+                    true}
+                else -> true
+            }
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_home, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
